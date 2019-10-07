9472382031cefd9c51809821c0162527c5415727
EndrioFelipe <endrio.souza@hotmail.com>
2019-10-07 18:02:10 -0300

    indo para heroku

--- mea/src/main/java/org/mea/config/JPAConfiguration.java
+++ mea/src/main/java/org/mea/config/JPAConfiguration.java
@@ -1,10 +1,15 @@
 package org.mea.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -14,23 +19,46 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 public class JPAConfiguration {
+	
+	@Autowired
+	private Environment env;
+	
 	@Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws URISyntaxException {
+
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 
         factoryBean.setJpaVendorAdapter(vendorAdapter);
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUsername("root");
+        
+        
+        //********************MYSQL******************************
+        //dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+       /* dataSource.setUsername("root");
         dataSource.setPassword("1035491020");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mea?useTimezone=true&serverTimezone=UTC");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mea?useTimezone=true&serverTimezone=UTC");*/
+        
+        
+        //************************POSTGRE**************************
+        URI dbUrl = new URI(env.getProperty("DATABASE_URL"));
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()
+        	+":"+dbUrl.getPort()+dbUrl.getPath());
+        dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
+        dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
 
         factoryBean.setDataSource(dataSource);
 
         Properties props = new Properties();
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+      //********************MYSQL******************************
+       // props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        
+      //************************POSTGRE**************************
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.postgreSQLDialect");
+        
+      //************************RESTANTE MANTEM**************************
         props.setProperty("hibernate.show_sql", "true");
         props.setProperty("hibernate.hbm2ddl.auto", "update");
         
